047d49cc74efb6f7dd822c0ea049b2db6e760adf
veritas <1820819809@qq.com>
2025-03-08 10:30:20 +0800

    Initialize main branch

---
+++ src/test/java/com/rpc/rpcself/RpcSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpc.rpcself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcSelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
